902bfca2cfeb2428bab4d4e6c7af0fc7aa8fe1f1
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-03-26 13:20:35 +0900

    실전 예제 3 - 다양한 연관관계 매핑 학습

--- 자바 ORM 표준 JPA 프로그래밍 -기본편/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
+++ 자바 ORM 표준 JPA 프로그래밍 -기본편/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,12 +1,12 @@
 package jpabook.jpashop.domain;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,5 +21,8 @@ public class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 } // end class
